f0e6d6f3072584d88617b5cc6cb58649c2bf0bc6
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2017-03-21 15:06:12 +0100

    Implemented a few model functions

--- app/models/shoppingService.js
+++ app/models/shoppingService.js
@@ -1,6 +1,47 @@
 
 shoppingApp.factory('ShoppingModel', function ($resource, $cookieStore) {
     
+    var shoppingCart =[];
+    var quantities= [];
     
-    
+    this.getShoppingCart = function (){
+        return shoppingCart;
+    }
+    this.addArticleShoppingCart = function (article){
+        shoppingCart.push(article);
+        quantities.push(1);
+    }
+        
+    this.removeArticle = function(article){
+        var index = shoppingCart.indexOf(article);
+        if(index>-1){
+            shoppingCart.splice(index,1);
+            quantities.splice(index,1);
+            
+        }
+    }
+    this.incrementArticleQuantity = function (article){
+        var index = shoppingCart.indexOf(article);
+        if(index>-1){
+            quantities[index]++;
+        }
+        
+    }
+    this.decrementArticleQuantity = function (article){
+        var index = shoppingCart.indexOf(article);
+        if(index>-1 && quantities[index]>0){
+            
+            quantities[index]--;
+        }
+        
+    }
+    this.getTotalPrice = function(){
+        var price = 0;
+		for(var i =0; i<shoppingCart.length; i++) {
+			price += shoppingCart[i];
+			
+		}
+		return price;
+        
+    }
 });
