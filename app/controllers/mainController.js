f99db34a2fa036147a438c9cb8d083fb97a33fe2
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2017-03-25 17:56:13 +0100

    Updated mainController

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,154 +1,154 @@
+
 shoppingApp.controller('MainController', function($scope, ShoppingModel){
     
-    
     // Filters
-$scope.filters = [
-    {
-        name: 'Color',
-        key: 'color',
-        values: [
-            {
-                name: 'Black',
-                value: 'black',
-                selected: false
-            },
-            {
-                name: 'White',
-                value: 'white',
-                selected: false
-            },
-            {
-                name: 'Grey',
-                value: 'gray',
-                selected: false
-            },
-            {
-                name: 'Blue',
-                value: 'blue',
-                selected: false
-            },
-            {
-                name: 'Red',
-                value: 'red',
-                selected: false
-            },
-            {
-                name: 'Multicolored',
-                value: 'multicolored',
-                selected: false
-            }
-        ]
-    },
-    {
-        name: 'Price',
-        key: 'price',
-        values: [
-            {
-                name: '< 15 £',
-                value: '0-15',
-                selected: false
-            },
-            {
-                name: '15 £ - 50 £',
-                value: '15-50',
-                selected: false
-            },
-            {
-                name: '50 £ - 100 £',
-                value: '50-100',
-                selected: false
-            },
-            {
-                name: '100 £ - 500 £',
-                value: '100-500',
-                selected: false
-            },
-            {
-                name: '> 500 £',
-                value: '500-100000',
-                selected: false
-            }
-        ]
-    },
-    {
-        name: 'Size',
-        key: 'size',
-        values: [
-            {
-                name: 'XS',
-                value: 'XS',
-                selected: false
-            },
-            {
-                name: 'S',
-                value: 'S',
-                selected: false
-            },
-            {
-                name: 'M',
-                value: 'M',
-                selected: false
-            },
-            {
-                name: 'L',
-                value: 'L',
-                selected: false
-            },
-            {
-                name: 'XL',
-                value: 'XL',
-                selected: false
-            }
-        ]
-    },
-    {
-        name: 'Collection',
-        key: 'season',
-        values: [
-            {
-                name: 'Spring/Summer',
-                value: 'summer',
-                selected: false
-            },
-            {
-                name: 'Autumn/Winter',
-                value: 'winter',
-                selected: false
-            }
-        ]
-    },
-    {
-        name: 'Pattern',
-        key: 'pattern',
-        values: [
-            {
-                name: 'Colored',
-                value: 'colored',
-                selected: false
-            },
-            {
-                name: 'Striped',
-                value: 'striped',
-                selected: false
-            },
-            {
-                name: 'Polka Dot',
-                value: 'polkaDot',
-                selected: false
-            },
-            {
-                name: 'Checkered',
-                value: 'checkered',
-                selected: false
-            },
-            {
-                name: 'Plain',
-                value: 'plain',
-                selected: false
-            }
-        ]
-    }
-];
+    $scope.filters = [
+        {
+            name: 'Color',
+            key: 'color',
+            values: [
+                {
+                    name: 'Black',
+                    value: 'black',
+                    selected: false
+                },
+                {
+                    name: 'White',
+                    value: 'white',
+                    selected: false
+                },
+                {
+                    name: 'Grey',
+                    value: 'gray',
+                    selected: false
+                },
+                {
+                    name: 'Blue',
+                    value: 'blue',
+                    selected: false
+                },
+                {
+                    name: 'Red',
+                    value: 'red',
+                    selected: false
+                },
+                {
+                    name: 'Multicolored',
+                    value: 'multicolored',
+                    selected: false
+                }
+            ]
+        },
+        {
+            name: 'Price',
+            key: 'price',
+            values: [
+                {
+                    name: '< 15 £',
+                    value: '0-15',
+                    selected: false
+                },
+                {
+                    name: '15 £ - 50 £',
+                    value: '15-50',
+                    selected: false
+                },
+                {
+                    name: '50 £ - 100 £',
+                    value: '50-100',
+                    selected: false
+                },
+                {
+                    name: '100 £ - 500 £',
+                    value: '100-500',
+                    selected: false
+                },
+                {
+                    name: '> 500 £',
+                    value: '500-100000',
+                    selected: false
+                }
+            ]
+        },
+        {
+            name: 'Size',
+            key: 'size',
+            values: [
+                {
+                    name: 'XS',
+                    value: 'XS',
+                    selected: false
+                },
+                {
+                    name: 'S',
+                    value: 'S',
+                    selected: false
+                },
+                {
+                    name: 'M',
+                    value: 'M',
+                    selected: false
+                },
+                {
+                    name: 'L',
+                    value: 'L',
+                    selected: false
+                },
+                {
+                    name: 'XL',
+                    value: 'XL',
+                    selected: false
+                }
+            ]
+        },
+        {
+            name: 'Collection',
+            key: 'season',
+            values: [
+                {
+                    name: 'Spring/Summer',
+                    value: 'summer',
+                    selected: false
+                },
+                {
+                    name: 'Autumn/Winter',
+                    value: 'winter',
+                    selected: false
+                }
+            ]
+        },
+        {
+            name: 'Pattern',
+            key: 'pattern',
+            values: [
+                {
+                    name: 'Colored',
+                    value: 'colored',
+                    selected: false
+                },
+                {
+                    name: 'Striped',
+                    value: 'striped',
+                    selected: false
+                },
+                {
+                    name: 'Polka Dot',
+                    value: 'polkaDot',
+                    selected: false
+                },
+                {
+                    name: 'Checkered',
+                    value: 'checkered',
+                    selected: false
+                },
+                {
+                    name: 'Plain',
+                    value: 'plain',
+                    selected: false
+                }
+            ]
+        }
+    ];
     
     $scope.searchResults = [];
     $scope.randomArticles = [];
@@ -210,6 +210,5 @@ $scope.filters = [
             $scope.searchResults.push(data);;
         });
     };
-    
     
 });
