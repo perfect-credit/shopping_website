2c35282ca0f46af7274c7518f5c97ec7282c791e
perfect-credit <185212043+perfect-credit@users.noreply.github.com>
2017-03-28 14:12:20 +0200

    Added routing rules to app.js and added home.html

--- app/app.js
+++ app/app.js
@@ -7,6 +7,24 @@ shoppingApp.config(['$routeProvider',
             when('/home', {
                 templateUrl: '/app/views/partials/home.html',
                 controller: 'MainController'
+            }).when('/article/:articleId', {
+                templateUrl: '/app/views/partials/article.html'/*,
+                controller: 'MainController'*/
+            }).when('/shoppingCart', {
+                templateUrl: '/app/views/partials/shoppingCart.html'/*,
+                controller: 'MainController'*/
+            }).when('/checkout', {
+                templateUrl: '/app/views/partials/checkout.html'/*,
+                controller: 'MainController'*/
+            }).when('/orderHistory', {
+                templateUrl: '/app/views/partials/orderHistory.html'/*,
+                controller: 'MainController'*/
+            }).when('/favorites', {
+                templateUrl: '/app/views/partials/favorites.html'/*,
+                controller: 'MainController'*/
+            }).when('/confirmation', {
+                templateUrl: '/app/views/partials/confirmation.html'/*,
+                controller: 'MainController'*/
             }).otherwise({
                 redirectTo: '/home'
             });
